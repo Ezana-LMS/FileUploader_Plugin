1735d2d8f1d2552014f46129d9f1c3c25209627a
Martin Mbithi <martdevelopers254@gmail.com>
2022-01-17 09:30:36 +0300

    Intergrate Google Drive API Keys

--- google_drive_api.php
+++ google_drive_api.php
@@ -1,22 +1,23 @@
 <?php
 session_start();
-$url_array = explode('?', 'http://'.$_SERVER ['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+$url_array = explode('?', 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 $url = $url_array[0];
 
 require_once 'google-api-php-client/src/Google_Client.php';
 require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
 $client = new Google_Client();
-$client->setClientId('Place your ClientId Here');
-$client->setClientSecret('Place your ClientSecret here');
+$client->setClientId('358941079342-dhdhsdjs700cdvs5u1mffppfe2teuqqt.apps.googleusercontent.com');
+$client->setClientSecret('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');
 $client->setRedirectUri($url);
 $client->setScopes(array('https://www.googleapis.com/auth/drive'));
 if (isset($_GET['code'])) {
     $_SESSION['accessToken'] = $client->authenticate($_GET['code']);
-    header('location:'.$url);exit;
+    header('location:' . $url);
+    exit;
 } elseif (!isset($_SESSION['accessToken'])) {
     $client->authenticate();
 }
-$files= array();
+$files = array();
 $dir = dir('files');
 while ($file = $dir->read()) {
     if ($file != '.' && $file != '..') {
@@ -30,10 +31,10 @@ if (!empty($_POST)) {
     $finfo = finfo_open(FILEINFO_MIME_TYPE);
     $file = new Google_DriveFile();
     foreach ($files as $file_name) {
-        $file_path = 'files/'.$file_name;
+        $file_path = 'files/' . $file_name;
         $mime_type = finfo_file($finfo, $file_path);
         $file->setTitle($file_name);
-        $file->setDescription('This is a '.$mime_type.' document');
+        $file->setDescription('This is a ' . $mime_type . ' document');
         $file->setMimeType($mime_type);
         $service->files->insert(
             $file,
@@ -44,6 +45,7 @@ if (!empty($_POST)) {
         );
     }
     finfo_close($finfo);
-    header('location:'.$url);exit;
+    header('location:' . $url);
+    exit;
 }
 include 'index.php';
